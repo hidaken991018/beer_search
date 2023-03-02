ab379e576d9a8554ba4b20a3fb6b27fa385fbc0d
hidaken991018 <ed.hidaken@gmail.com>
2023-03-02 23:05:10 +0900

    ファイル整理・コメント付与

--- src/components/atoms/sharing-button/SNSButton.tsx
+++ src/components/atoms/sharing-button/SNSButton.tsx
@@ -1,21 +1,28 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image';
+import Link from 'next/link';
 
 type Props = {
-    iconImagePath: string;
-    linkUrl:string;
-}
+  iconImagePath: string;
+  linkUrl: string;
+};
 
-export const TwitterShare = (props: Props) => {
-    const { iconImagePath, linkUrl} = props;
+/**
+ * SNSシェアボタン
+ * @param props
+ * @returns
+ */
+export const SNSButton = (props: Props) => {
+  const { iconImagePath, linkUrl } = props;
 
-    // const shareToSNS = `https://twitter.com/share?url=${shareUrl}&text=${shareText}&hashtag=${shareHashtag}`
-    return<>
-    {/* TODO Twitter画像 */}
-    <Link href={linkUrl}>
-        <a target={"_blank"}>
-            <Image src={iconImagePath} alt="" layout="fill"/>
+  // const shareToSNS = `https://twitter.com/share?url=${shareUrl}&text=${shareText}&hashtag=${shareHashtag}`
+  return (
+    <>
+      {/* TODO Twitter画像 */}
+      <Link href={linkUrl}>
+        <a target={'_blank'}>
+          <Image src={iconImagePath} alt="" layout="fill" />
         </a>
-    </Link>
+      </Link>
     </>
-}
+  );
+};
--- src/components/ui/Selector.tsx
+++ src/components/ui/Selector.tsx
@@ -1,42 +1,39 @@
 import * as React from 'react';
-import FormatBoldIcon from '@mui/icons-material/FormatBold';
-import FormatItalicIcon from '@mui/icons-material/FormatItalic';
-import FormatUnderlinedIcon from '@mui/icons-material/FormatUnderlined';
-import FormatColorFillIcon from '@mui/icons-material/FormatColorFill';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
-import { createTheme } from '@mui/material/styles';
 import { useState } from 'react';
 
 type Props = {
-  selector: string[]
-}
+  selector: string[];
+};
 
 export const Selector = (props: Props) => {
   const { selector } = props;
-  const [formats, setFormats] = useState<string[]>(() => [""]);
+  const [formats, setFormats] = useState<string[]>(() => ['']);
 
   const handleFormat = (
     event: React.MouseEvent<HTMLElement>,
-    newFormats: string[],
+    newFormats: string[]
   ) => {
     setFormats(newFormats);
   };
-
 
   return (
     <ToggleButtonGroup
       value={formats}
       onChange={handleFormat}
       aria-label="text formatting"
-      style={{ display: "flex", flexWrap: "wrap" }}
+      style={{ display: 'flex', flexWrap: 'wrap' }}
     >
       {selector.map((style, index) => (
-        <ToggleButton value={style + index} aria-label={style + index} key={index}>
+        <ToggleButton
+          value={style + index}
+          aria-label={style + index}
+          key={index}
+        >
           {style}
         </ToggleButton>
       ))}
     </ToggleButtonGroup>
   );
-}
+};
